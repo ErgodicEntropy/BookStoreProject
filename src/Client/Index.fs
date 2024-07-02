0ad1f19d0bd4e94d825d720da7903b6bbb908d33
ErgodicEntropy <129313892+ErgodicEntropy@users.noreply.github.com>
2024-07-02 13:50:22 +0100

    Add files via upload

---
+++ src/Client/Index.fs
@@ -0,0 +1,146 @@
+module Index
+
+open System
+open Browser
+open Elmish
+open Fable.Remoting.Client
+open Feliz.DaisyUI
+open Feliz.Router
+open Page
+open Shared
+
+type PageTab =
+    | Home
+    | Login
+    | Wishlist of UserData
+    | NotFound
+
+type User =
+    | Guest
+    | User of UserData
+
+type Model = { Page: PageTab; User: User }
+
+type Msg =
+    | UrlChanged of string list
+    | OnSessionChange
+    | Logout
+
+
+let guestApi =
+    Remoting.createApi ()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.buildProxy<IGuestApi>
+
+let initFromUrl model url =
+    let loggedInUser =
+        Session.loadUser () |> Option.map User |> Option.defaultValue Guest
+
+    match url with
+    | [] ->
+        let model = { Page = Home; User = loggedInUser }
+        model, Cmd.none
+    | [ "login" ] ->
+        let model = { Page = Login; User = loggedInUser }
+        model, Cmd.none
+    | [ "wishlist" ] ->
+        match loggedInUser with
+        | User user ->
+            let model = {
+                Page = Wishlist user
+                User = loggedInUser
+            }
+
+            model, Cmd.none
+        | Guest -> model, Cmd.navigate "login"
+    | _ -> { Page = NotFound; User = loggedInUser }, Cmd.none
+
+let init () =
+    let user = Session.loadUser () |> Option.map User |> Option.defaultValue Guest
+
+    Router.currentUrl () |> initFromUrl { Page = Home; User = user }
+
+let update msg model =
+    match msg with
+    | UrlChanged url -> initFromUrl model url
+    | Logout ->
+        Session.deleteUser ()
+        { model with User = Guest }, Cmd.navigate ""
+    | OnSessionChange ->
+        let session = Session.loadUser ()
+        let user = session |> Option.map User |> Option.defaultValue Guest
+
+        let cmd =
+            session
+            |> Option.map (fun _ -> Cmd.none)
+            |> Option.defaultValue (Cmd.navigate "login")
+
+        { model with User = user }, cmd
+
+open Feliz
+
+let logo =
+    Html.a [
+        prop.href "https://safe-stack.github.io/"
+        prop.className "ml-12 h-12 w-12 bg-primary hover:cursor-pointer hover:bg-teal-400"
+        prop.children [ Html.img [ prop.src "/favicon.png"; prop.alt "Logo" ] ]
+    ]
+
+let navigation model dispatch =
+    Html.div [
+        prop.className "grid"
+        prop.children [
+            Daisy.tabs [
+                prop.className "justify-self-center"
+                prop.children [
+                    Daisy.tab [ prop.text "Home"; prop.onClick (fun _ -> Router.navigate "") ]
+                    match model.User with
+                    | Guest -> Daisy.tab [ prop.text "Login"; prop.onClick (fun _ -> Router.navigate "login") ]
+                    | User user ->
+                        Daisy.tab [ prop.text "Wishlist"; prop.onClick (fun _ -> Router.navigate "wishlist") ]
+                        Daisy.tab [ prop.text "Logout"; prop.onClick (fun _ -> dispatch Logout) ]
+                        Daisy.tab [ prop.text $"Logged in as {user.UserName.Value}" ]
+                ]
+            ]
+        ]
+    ]
+
+let view model dispatch =
+    React.router [
+        router.onUrlChanged (UrlChanged >> dispatch)
+        router.children [
+            Html.section [
+                theme.winter
+                prop.className "grid grid-rows-index gap-5 h-screen"
+                prop.children [
+                    logo
+                    navigation model dispatch
+                    Daisy.divider ""
+                    Html.div [
+                        prop.className "overflow-y-auto"
+                        prop.children [
+                            match model.Page with
+                            | Home -> Home.View guestApi
+                            | Login -> Login.View guestApi
+                            | Wishlist user -> WishList.View user
+                            | NotFound -> Html.div [ prop.text "Not Found" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let resetStorage onResetStorageMsg =
+    let register dispatch =
+        let callback _ = dispatch onResetStorageMsg
+        window.addEventListener ("storage", callback)
+
+        { new IDisposable with
+            member _.Dispose() =
+                window.removeEventListener ("storage", callback)
+        }
+
+    register
+
+let subscribe _ = [ [ "resetStorage" ], resetStorage OnSessionChange ]
---
+++ src/Client/pages/Home.fs
@@ -0,0 +1,64 @@
+module Page.Home
+
+open Elmish
+open Feliz.DaisyUI
+open Feliz.UseElmish
+open Shared
+
+type Model = { Books: Book seq }
+
+type Msg = GotBooks of Book seq
+
+let init (guestApi: IGuestApi) =
+    let model = { Books = Seq.empty }
+    let cmd = Cmd.OfAsync.perform guestApi.getBooks () GotBooks
+    model, cmd
+
+let update msg model =
+    match msg with
+    | GotBooks books -> { Books = books }, Cmd.none
+
+open Feliz
+
+let bookRow book =
+    let link =
+        Daisy.link [
+            link.hover
+            link.primary
+            prop.target "_blank"
+            prop.text book.Title
+            prop.href book.Link
+        ]
+
+    let image = Html.img [ prop.src book.ImageLink ]
+
+    let tableCell (key: string) (element: ReactElement) =
+        Html.td [ prop.key key; prop.children element ]
+
+    Html.tr [
+        prop.className "hover:primary"
+        prop.children [
+            tableCell "title" link
+            tableCell "authors" (Html.text book.Authors)
+            tableCell "image" image
+        ]
+    ]
+
+[<ReactComponent>]
+let View api =
+    let model, dispatch = React.useElmish ((fun () -> init api), update, [||])
+
+    Html.div [
+        prop.className "overflow-y-auto"
+        prop.children [
+            Daisy.table [
+                prop.children [
+                    Html.tbody [
+                        for book in model.Books do
+                            bookRow book
+                    ]
+                    Html.thead [ Html.tr [ Html.th "Title"; Html.th "Authors"; Html.th "Image" ] ]
+                ]
+            ]
+        ]
+    ]
